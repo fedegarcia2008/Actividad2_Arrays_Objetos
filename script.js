c72c4439c78aa13487073a0086f4aa37bc526960
fedegarcia2008 <garcia.federico@tecnica7.edu.ar>
2026-06-08 00:08:18 -0300

    Buscar presupuesto

--- script.js
+++ script.js
@@ -27,8 +27,6 @@ document.getElementById('ing').addEventListener('click', () => {
     presup.cuotas_veinticuatro = presup.pre_inter / 24;
 
     presup.cuotas_treintayseis = presup.pre_inter / 36;
-
-    console.log(presup);
     
     const salida = document.getElementById('salida');
     salida.innerHTML = '';
@@ -108,4 +106,50 @@ document.getElementById('ing').addEventListener('click', () => {
         salida.appendChild(tabla);
     }
     precioMayor();
+});
+
+document.getElementById('buscar').addEventListener('click', () => {
+    const salida = document.getElementById('salidaBusqueda');
+    salida.innerHTML = '';
+    function buscar() {
+        let buscado = document.getElementById('busCliente').value;
+        let presupuesto = datos.filter(pre =>
+            pre.cli.toLowerCase() === buscado.toLowerCase()
+        );
+        if (presupuesto.length > 0) {
+            let tabla = document.createElement('table');
+            let h2 = document.createElement('h2');
+            h2.textContent = `Presupuesto del cliente ${buscado}`;
+            tabla.innerHTML = `
+            <tr>
+                <th>MARCA</th>
+                <th>PRECIO</th>
+                <th>IVA 21%</th>
+                <th>PRECIO CONTADO</th>
+                <th>INTERÉS 10%</th>
+                <th>PRECIO CON INTERÉS</th>
+                <th>VALOR EN 24 CUOTAS</th>
+                <th>VALOR EN 36 CUOTAS</th>
+            </tr>`;
+            presupuesto.forEach(presup => {
+                let fila = document.createElement('tr');
+                fila.innerHTML = `
+                <td>${presup.marc}</td>
+                <td>$${presup.pre}</td>
+                <td>$${presup.iva.toFixed(2)}</td>
+                <td>$${presup.cont.toFixed(2)}</td>
+                <td>$${presup.inter.toFixed(2)}</td>
+                <td>$${presup.pre_inter.toFixed(2)}</td>
+                <td>$${presup.cuotas_veinticuatro.toFixed(2)}</td>
+                <td>$${presup.cuotas_treintayseis.toFixed(2)}</td>
+                `;
+                tabla.appendChild(fila);
+            });
+            salida.appendChild(h2);
+            salida.appendChild(tabla);
+        } else {
+            salida.innerHTML = '<h3>No se encontraron presupuestos para ese cliente</h3>';
+        }
+    }
+    buscar();
 });
