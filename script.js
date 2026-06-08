ae6ae3fd3e90b400f649147c7fe51908c333f445
fedegarcia2008 <garcia.federico@tecnica7.edu.ar>
2026-06-07 21:22:42 -0300

    Interés y Precio Interés

--- script.js
+++ script.js
@@ -4,21 +4,25 @@ document.getElementById('ing').addEventListener('click', () => {
 
     let presup = {
         marc: document.getElementById('m').value,
-        prec: parseInt(document.getElementById('p').value),
+        pre: parseInt(document.getElementById('p').value),
         cli: document.getElementById('c').value,
         iva: 0,
         cont: 0,
         inter: 0,
-        prec_inter: 0,
+        pre_inter: 0,
         cuotas_veinticuato: 0,
         cuotas_treintayseis: 0
     };
 
     datos.push(presup);
 
-    presup.iva = presup.prec * 21;
+    presup.iva = presup.pre * 0.21;
 
-    presup.cont = presup.prec + presup.iva;
+    presup.cont = presup.pre + presup.iva;
+
+    presup.inter = presup.cont * 0.10;
+
+    presup.pre_inter = presup.cont + presup.inter;
 
     console.log(presup);
 });
